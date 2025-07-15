687ee5903094a24ddf400dc4bd3e49ad235f9ebd
adi7392 <adiiigupta7392@gmail.com>
2025-07-16 00:24:14 +0530

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Component1 from './Component1'
+import Component2 from './Component2'
+import Component3 from './Component3'
+
+const App = () => {
+  return (
+    <div> 
+    <Component1 />
+    <Component1 />
+    <Component2 />
+    <Component3 />
+    </div>
+  )
+}
+
+export default App
