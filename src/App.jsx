49d7ca8c56be81a0ceb721f938e1e64f770afb8c
adi7392 <adiiigupta7392@gmail.com>
2025-07-16 00:38:13 +0530

    cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,8 @@
 import React from 'react'
-import Component1 from './Component1'
-import Component2 from './Component2'
-import Component3 from './Component3'
-
 const App = () => {
   return (
     <div> 
-    <Component1 />
-    <Component1 />
-    <Component2 />
-    <Component3 />
+      this is my app
     </div>
   )
 }
--- src/Component3.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Component3 = () => {
-  return (
-    <div><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis porro perspiciatis commodi doloribus, praesentium, esse deserunt voluptatibus voluptatem pariatur nisi eius eligendi corrupti at recusandae ducimus in ea sint animi natus. Deserunt id cum repellat fugit blanditiis, quidem debitis excepturi eaque vitae sapiente consectetur necessitatibus corrupti exercitationem atque numquam culpa aliquam in accusamus! Molestias fugit sunt, minima, at, nihil voluptatum temporibus consequuntur recusandae pariatur debitis necessitatibus! Repellendus fuga iure possimus facere pariatur necessitatibus, tempora ducimus id perspiciatis natus dolores, harum repellat nulla quam alias? Fugiat voluptatem beatae illo dolore qui expedita. Ullam voluptatem saepe nostrum aut sapiente. Sapiente, odit maxime?</p></div>
-  )
-}
-
-export default Component3
